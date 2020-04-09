de3f68b040d74d5540e4f97d849acc41634c7a87
Katie Moore <kdmoore@email.wm.edu>
2020-04-09 12:53:28 -0400

    Add files via upload

---
+++ holmes.py
@@ -0,0 +1,13 @@
+def main():
+    print("""\n \"But one false statement was made by Barrymore at the inquest. He said that there were no traces upon the ground round the body. He did not observe any. But I did - some little distance off, but fresh and clear.\"\n""")
+    
+    print('\"Footprints?\"\n')
+
+
+    print('\"Footprints.\"\n')
+
+
+    print('\"A man\'s or a woman\'s?\"\n')
+
+if __name__== '__main__':
+    main()
